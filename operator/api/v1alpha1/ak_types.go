bf7fc8f1a68ae75a381caa7b58d127f195c167f5
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2023-02-15 10:18:22 +0000

    Added Kubebuilder validation package flags

--- operator/api/v1alpha1/ak_types.go
+++ operator/api/v1alpha1/ak_types.go
@@ -20,8 +20,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+//+kubebuilder:validation:Optional
 
 // AkSpec defines the desired state of Ak
 type AkSpec struct {
