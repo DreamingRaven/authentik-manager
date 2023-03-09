b7e79e9afa725beaa8e1d2b33e7677056e00bb49
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2023-03-09 13:42:47 +0000

    BP API spec

--- operator/api/v1alpha1/akblueprint_types.go
+++ operator/api/v1alpha1/akblueprint_types.go
@@ -11,19 +11,70 @@ You may obtain a copy of the License in the project root (LICENSE) or at
 package v1alpha1
 
 import (
+	"encoding/json"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
-
 // AkBlueprintSpec defines the desired state of AkBlueprint
 type AkBlueprintSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of AkBlueprint. Edit akblueprint_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Blueprint is a container for a complete single authentik blueprint yaml spec
+	Blueprint BP `json:"blueprint,omitempty"`
+}
+
+// BP is a whole blueprint struct containing the full structure of an authentik blueprint
+// https://goauthentik.io/developer-docs/blueprints/v1/structure#structure
+type BP struct {
+	// Version is the version of this blueprint
+	Version int `json:"version"`
+
+	// Metadata block specifying labels and names of the blueprint
+	Metadata BPMeta `json:"metadata"`
+
+	// Context (optional) authentik default context (whatever that means)
+	Context json.RawMessage `json:"context,omitempty"`
+
+	// Entries lists models we want to use via this blueprint
+	Entries []BPModel `json:"entries"`
+}
+
+// BPMeta is the metadata of an authentik blueprint as authentik likes
+type BPMeta struct {
+
+	// Labels (optional) ke-value store for special labels
+	// https://goauthentik.io/developer-docs/blueprints/v1/structure#special-labels
+	Labels json.RawMessage `json:"labels,omitempty"`
+
+	// Name of the authentik blueprint for authentik to register
+	Name string `json:"name"`
+}
+
+// BPModel is a rough outline of the structure of models authentik likes in its blueprints
+type BPModel struct {
+
+	// Model "app.model" notation of which model from authentik to call
+	Model string `json:"model"`
+
+	//+kubebuilder:validation:Enum="present";"create";"absent"
+	//+kubebuilder:default:=present
+	// State (optional) desired state of this model when loaded from "present", "create", "absent"
+	// present: (default) keeps the object in sync with its definition in this blueprint
+	// create: only creates the initial object with its values here
+	// absent: deletes the object
+	State string `json:"state,omitempty"`
+
+	// Conditions (optional) a list of conditions which if all match the model will be activated. If not the model will be inactive
+	Conditions []string `json:"conditions,omitempty"`
+
+	// Identifiers key-value identifiers to allow filtering of this stage, and identifying it
+	Identifiers json.RawMessage `json:"identifiers"`
+
+	// Id (optional) is similar to identifiers except is optional and is just an ID to reference this model using !KeyOf syntax in authentik
+	Id string `json:"id,omitempty"`
+
+	// Attrs is a list of settings / options / overrides of the defaults of this model
+	Attrs json.RawMessage `json:"attrs,omitempty"`
 }
 
 // AkBlueprintStatus defines the observed state of AkBlueprint
