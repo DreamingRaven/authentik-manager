fdf336fa8c1bc690cce4efb53db5224e657ed937
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2023-06-07 17:16:49 +0100

    Added further outlines and comments

--- operator/utils/control-base.go
+++ operator/utils/control-base.go
@@ -1,3 +1,4 @@
+// Package utils implements various utilities  for general use in our controllers.
 package utils
 
 import (
@@ -5,12 +6,24 @@ import (
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
-// ControlBase struct centralises common controller functions into an embeded base struct
-// to make the functions available with as little repettition as possible
+// ControlBase struct centralises common controller functions into an embedded base struct
+// to make the functions available with as little repetition as possible.
 // https://stackoverflow.com/a/31505875
 type ControlBase struct {
 	client.Client
 	Scheme *runtime.Scheme
 }
 
+// Control composes additional functionality we would like available to our controllers.
+// This functionality is key to ensuring we KISS, and implements common routines
+// like searching namespaces for resources or lists, along with common transformations.
+// This does not include functions that do not require client or scheme context
+// since those are better as standalone implementations rather than bundled routines.
 type Control interface {}
+
+// ListInNamespace lists resources of given group, version, kind in the given namespace.
+func (c *ControlBase) ListInNamespace() {}
+
+func (c *ControlBase) GetReleaseValues() {}
+
+// HELM routines
