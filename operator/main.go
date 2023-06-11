81454ffd3fc28306906795aec57442a784ae2472
GeorgeRaven <GeorgeRavenCommunity@pm.me>
2023-06-11 15:42:39 +0100

    Added OIDC API

--- operator/main.go
+++ operator/main.go
@@ -98,6 +98,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "AkBlueprint")
 		os.Exit(1)
 	}
+	if err = (&controllers.OIDCReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OIDC")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
